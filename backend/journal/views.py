9fe19a630362b517ccf044f08bbbabd3326567ca
Noi Koffi <noikofi.nk@gmail.com>
2024-07-08 16:20:59 +0300

    Added CategoryViewSet and JournalViewSet

--- backend/journal/views.py
+++ backend/journal/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Category, Journal
+from .serializers import CategorySerializer, JournalSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class JournalViewSet(viewsets.ModelViewSet):
+    queryset = Journal.objects.all()
+    serializer_class = JournalSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.IsOwnerOrReadOnly]
+
+
+
