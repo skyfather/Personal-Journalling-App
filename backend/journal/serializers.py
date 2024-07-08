27399902f5e02a6e14a84cbb25c183b87a925c18
Noi Koffi <noikofi.nk@gmail.com>
2024-07-08 16:07:15 +0300

    Added serializer class for Category and Journal

---
+++ backend/journal/serializers.py
@@ -0,0 +1,14 @@
+from .models import Category, Journal
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class JournalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Journal
+        fields= '__all__'
+        read_only_fields = ['owner']
